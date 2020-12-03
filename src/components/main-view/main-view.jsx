f2bd70592a46deaae0a8d47ca2b5d4cbfb4f2a34
root <root@DESKTOP-VUKLK3J.localdomain>
2020-12-03 20:36:18 +0100

    update main-view, movie-card and index.html

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import axios from "axios";
 import PropTypes from "prop-types";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Navbar from "react-bootstrap/Navbar";
+import { Nav } from "react-bootstrap";
 
 import {RegistrationView} from "../registration-view/registration-view";
 import {LoginView} from "../login-view/login-view";
@@ -60,19 +64,33 @@ export class MainView extends React.Component {
 
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
 
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    //if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view">LOADING</div>;
 
     return (
     <div className="main-view">
-      {selectedMovie
-        ? <MovieView movie={selectedMovie}/>
-        : movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-        ))
-      }
+      <Container>
+      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+              <Navbar.Brand href="#home">MARVELIX</Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="">Profile</Nav.Link>
+                  <Nav.Link href="">Log Out</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+        <Row>
+          {selectedMovie
+          ? <MovieView movie={selectedMovie}/>
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+          ))
+          }
+        </Row>
+      </Container>
     </div>
     );
   }
