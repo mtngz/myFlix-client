052ce05f4665e35158bb28b2798ca5ff1f548c72
root <root@DESKTOP-VUKLK3J.localdomain>
2020-12-08 17:07:57 +0100

    update

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -12,7 +12,7 @@ export function LoginView(props) {
   const handleSubmit = (e) => {
     e.preventDefault();
     /* Send a request to the server for authentication */
-    axios.post("https://marvelix.herokuapp.com/login", {
+    axios.post(`https://marvelix.herokuapp.com/login`, {
       Username: username,
       Password: password
     })
