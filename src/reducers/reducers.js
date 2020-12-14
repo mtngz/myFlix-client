66f5ae567e5b24ebf6c7f0b873294c6620c5ea01
root <root@DESKTOP-VUKLK3J.localdomain>
2020-12-14 15:50:30 +0100

    create reducers.js

---
+++ src/reducers/reducers.js
@@ -0,0 +1,36 @@
+import { combineReducers } from "redux";
+
+import { SET_FILTER, SET_MOVIES } from "../actions/actions.js";
+
+function visibilityFilter(state = "", action) {
+  switch (action.type) {
+    case SET_FILTER:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+function movies(state = [], action) {
+  switch (action.type) {
+    case SET_MOVIES:
+      return action.value;
+    default:
+      return state;
+  }
+}
+
+/* USE COMBINE REDUCERS INSTEAD
+function moviesApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    movies: movies(state.movies, action)
+  }
+}*/
+
+const moviesApp = combineReducers({
+  visibilityFilter,
+  movies,
+});
+
+export default moviesApp;
